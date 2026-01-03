ee4fac5d7ef6a5f9d79ac7140c8e83a7c00157dc
Reya888 <reyadutta863@gmail.com>
2026-01-03 15:43:44 +0530

    AddDbContext

--- dbOperationEFCORE/dbOperationEFCORE/Program.cs
+++ dbOperationEFCORE/dbOperationEFCORE/Program.cs
@@ -1,4 +1,7 @@
 
+using dbOperationEFCORE.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace dbOperationEFCORE
 {
     public class Program
@@ -8,6 +11,9 @@ namespace dbOperationEFCORE
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("AppDb")
+                ));
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
